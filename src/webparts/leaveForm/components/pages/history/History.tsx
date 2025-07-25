8ffad1c614597fc8ce2442ee56839986735da18e
Murali <muraliram728@gmail.com>
2025-07-25 13:27:19 +0530

    added history component

--- src/webparts/leaveForm/components/pages/history/History.tsx
+++ src/webparts/leaveForm/components/pages/history/History.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
+import './History.css'
+// import LeaveHistory from './LeaveHistory'
 
-const History =()=>{
-    return(
+const History = () => {
+    return (
         <>
-        <h1>This is History Content</h1>
+            <div className="parent-div">
+                <div className="box1">
+                    <span className="num">3</span>
+                    <span>Approved</span>
+                </div>
+                <div className="box2">
+                    <span className="num">1</span>
+                    <span>Pending</span>
+                </div>
+                <div className="box3">
+                    <span className="num">2</span>
+                    <span>Reject</span>
+                </div>
+                <div className="box4">
+                    <span className="num">5</span>
+                    <span>Total Requests</span>
+                </div>
+            </div>
+
         </>
     )
 }
